45574d68cf47b62f392b3eb383d0ef12281efce8
Andrew <snelling.andrew@gmail.com>
2021-03-16 14:19:54 +0100

    user registration started

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('role')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->binary('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/js/pages/auth/Register.js
+++ resources/js/pages/auth/Register.js
@@ -1,10 +1,67 @@
-import React from "react";
-import { Container, Form, FormControl, InputGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+    Button,
+    Container,
+    Form,
+    FormControl,
+    InputGroup,
+} from "react-bootstrap";
 
 const Register = () => {
+    const [
+        { email, username, password, password_confirmation },
+        setValues,
+    ] = useState({
+        email: "",
+        username: "",
+        password: "",
+        password_confirmation: "",
+    });
+    const [errors, setErrors] = useState(null);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        let request_data = { email, username, password, password_confirmation };
+        const response = await fetch("/register", {
+            method: "POST",
+            body: JSON.stringify(request_data),
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                "X-CSRF-TOKEN": document
+                    .querySelector('meta[name="csrf-token"]')
+                    .getAttribute("content"),
+            },
+        });
+        const response_data = await response.json();
+
+        if (response.status == 200) {
+            location.href = "/login";
+        } else {
+            setErrors(response_data.errors);
+        }
+    };
+
+    const handleChange = (event) => {
+        const allowed_names = [
+                "username",
+                "email",
+                "password",
+                "password_confirmation",
+            ],
+            name = event.target.name,
+            value = event.target.value;
+
+        if (-1 !== allowed_names.indexOf(name)) {
+            setValues((prev_values) => {
+                return { ...prev_values, [name]: value };
+            });
+        }
+    };
     return (
         <Container>
-            <Form>
+            <Form action="/register" method="post" onSubmit={handleSubmit}>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>Username</Form.Label>
                     <InputGroup className="mb-2">
@@ -12,21 +69,34 @@ const Register = () => {
                             <InputGroup.Text>🌄</InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
-                            id="inlineFormInputGroup"
-                            placeholder="HikeCzech"
+                            // id="inlineFormInputGroup"
+                            // placeholder="HikeCzech"
+                            type="text"
+                            value={username}
+                            name="username"
+                            onChange={handleChange}
                         />
                     </InputGroup>
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control
+                        type="email"
+                        // placeholder="name@example.com"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
                 <>
                     <Form.Label htmlFor="inputPassword5">Password</Form.Label>
                     <Form.Control
                         type="password"
-                        id="inputPassword5"
+                        // id="inputPassword5"
                         aria-describedby="passwordHelpBlock"
+                        value={password}
+                        name="password"
+                        onChange={handleChange}
                     />
                     <Form.Text id="passwordHelpBlock" muted>
                         Your password must be 8-20 characters long, contain
@@ -37,10 +107,16 @@ const Register = () => {
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control
-                        type="password_confirmation"
-                        placeholder="Password"
+                        type="password"
+                        // placeholder="Confirm Password"
+                        value={password_confirmation}
+                        name="password_confirmation"
+                        onChange={handleChange}
                     />
                 </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Form>
         </Container>
     );
