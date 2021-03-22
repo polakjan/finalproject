904f963fd96ab840b6e4809d5b6948f5bc00e126
Andrew <snelling.andrew@gmail.com>
2021-03-23 00:38:59 +0100

    users/comments factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use App\Models\Entity;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $users = User::all()->count();
+        $entities = Entity::all()->count();
+
+        return [
+            //
+            'user_id' => User::factory(),
+            'entity_id' => $this->faker->numberBetween(1, $entities),
+            'comment' => $this->faker->sentence,
+            'created_at' => $this->faker->dateTimeThisDecade('+0 years')
+        ];
+    }
+}
