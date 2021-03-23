38fc60d0ceef73f9a19f5a3d177f4d0dc4a91e16
Andrew <snelling.andrew@gmail.com>
2021-03-23 09:43:39 +0100

    entity factory still not working

--- database/factories/EntityFactory.php
+++ database/factories/EntityFactory.php
@@ -22,16 +22,16 @@ class EntityFactory extends Factory
      */
     public function definition()
     {
-        // $faker_address = \Faker\Factory::create('cs_CZ');
+        $faker_address = \Faker\Factory::create('cs_CZ');
         // dd($faker_address->region());
         // $random = mt_rand(1, 100);
 
         return [
             //
             'name' => $this->faker->words(4),
-            // 'region' => array($faker_address->region()),
+            // 'region' => $faker_address->region(),
             // 'region' => $this->faker->word(),
-            'region' => 'czechia',
+            // 'region' => 'czechia',
             // 'photo' => 'https://picsum.photos/id/' . $random . '/1200/900',
             'description' => $this->faker->paragraph(2),
             'created_at' => $this->faker->dateTimeThisDecade('+0 years'),
